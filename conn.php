badde24442c68c97c51e17cd5c905089d2667543
Lyves17 <nosprojets2024@gmail.com>
2025-09-08 18:08:23 +0000

    Fix connexion MySQL AlwaysData

--- conn.php
+++ conn.php
@@ -1,4 +1,14 @@
+<?php
 $servername = "mysql-goldenaxe.alwaysdata.net"; // toujours cette forme pour AlwaysData
-$username   = "goldenaxe";                     // ton utilisateur MySQL
-$password   = "H95yks@uCXeqSiJ";             // mot de passe MySQL
-$dbname     = "goldenaxe_db";             // nom complet de la base
+$username   = "goldenaxe";                      // ton utilisateur MySQL
+$password   = "H95yks@uCXeqSiJ";                // mot de passe MySQL
+$dbname     = "goldenaxe_db";                   // nom complet de la base
+
+// Créer la connexion
+$con = mysqli_connect($servername, $username, $password, $dbname);
+
+// Vérifier la connexion
+if (!$con) {
+    die("Connexion échouée : " . mysqli_connect_error());
+}
+?>
